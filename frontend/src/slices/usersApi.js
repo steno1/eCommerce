0d192b07ca6446a0d3b61e0c02f7212d06e0a0c7
steno1 <onuprinceley@gmail.com>
2023-09-28 05:18:12 +0100

    updated userApiSlice

--- frontend/src/slices/usersApi.js
+++ frontend/src/slices/usersApi.js
@@ -1,7 +1,12 @@
 // Import necessary constants and modules
 
-import { USERS_URL } from "../constant"; // Importing the URL constant for products
-import { apiSlice } from "./apiSlice"; // Importing the apiSlice module
+// Importing the URL constant for users
+
+import { USERS_URL } from "../constant";
+import { apiSlice } from "./apiSlice";
+
+// Importing the apiSlice module
+
 
 // Create a usersApiSlice using apiSlice.injectEndpoints
 export const usersApiSlice = apiSlice.injectEndpoints({
@@ -14,41 +19,50 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         body: data, // The request body containing user login data
       }),
     }),
-   // Define an endpoint to handle user logout
-   logout: builder.mutation({
-    query: (data) => ({
-      url: `${USERS_URL}/logout`, // The URL for sending a POST request for user logout
-      method: "POST", // HTTP method for the request (POST in this case)
-      body: data, // The request body containing user logout data
+
+    // Define an endpoint to handle user logout
+    logout: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/logout`, // The URL for sending a POST request for user logout
+        method: "POST", // HTTP method for the request (POST in this case)
+        body: data, // The request body containing user logout data
+      }),
     }),
-  }),
 
-  register:builder.mutation({
-    query:(data)=>({
-      url:`${USERS_URL}`,
-      method:"POST",
-      body:data
-    })
-  }),
-  profile:builder.mutation({
-query:(data)=>({
-  url:`${USERS_URL}/profile`,
-  method:"PUT",
-  body:data
+    // Define an endpoint to handle user registration
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`, // The URL for sending a POST request for user registration
+        method: "POST", // HTTP method for the request
+        body: data, // The request body containing user registration data
+      }),
+    }),
 
-})
-  }),
-  getUsers:builder.query({
-query:()=>({
-  url:USERS_URL,
-}),
-providesTags:["Users"],
-keepUnusedDataFor:5
-  })
+    // Define an endpoint to handle updating user profile
+    profile: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`, // The URL for sending a PUT request for updating user profile
+        method: "PUT", // HTTP method for the request (PUT in this case)
+        body: data, // The request body containing user profile data
+      }),
+    }),
+
+    // Define an endpoint to fetch all users
+    getUsers: builder.query({
+      query: () => ({
+        url: USERS_URL, // The URL for fetching all users
+      }),
+      providesTags: ["Users"], // Cache tag for users data
+      keepUnusedDataFor: 5, // Keep unused data for 5 seconds
+    }),
   }),
 });
 
-// Extract the generated query hook from usersApiSlice
-export const { useLoginMutation,
-   useLogoutMutation, useRegisterMutation,
-    useProfileMutation, useGetUsersQuery} = usersApiSlice;
+// Extract the generated query and mutation hooks from usersApiSlice
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useProfileMutation,
+  useGetUsersQuery
+} = usersApiSlice;
